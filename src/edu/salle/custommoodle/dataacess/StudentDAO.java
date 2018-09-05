54996d7de18d05bf6c9b611f41daad0081cb65fb
Jorge Andrei <andreiarias53@gmail.com>
2018-09-05 17:56:59 -0500

    Initial Commit

---
+++ src/edu/salle/custommoodle/dataacess/StudentDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.salle.custommoodle.dataacess;
+
+import edu.salle.custommoodle.model.Student;
+import java.util.List;
+
+/**
+ *
+ */
+public interface StudentDAO {
+    public Student save(Student student);
+    public List<Student> findAll();
+    public Student find(String id);
+    
+    public Student findByLastName(String name);
+    
+    public void delete(Student student);
+    
+    public void update(Student student);
+    
+    
+    
+}
---
+++ src/edu/salle/custommoodle/model/Student.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.salle.custommoodle.model;
+
+import java.util.Objects;
+
+/**
+ *
+ */
+public class Student{
+    private String Name;
+    private String LastName;
+    private String Id;
+
+    public Student(String Name, String LastName) {
+        this.Name = Name;
+        this.LastName = LastName;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Id=%s,\nName=%s,\nLastName=%s",Id,Name,LastName); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public int hashCode(){
+        return Id.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (!Objects.equals(this.Id, other.Id)) {
+            return false;
+        }
+        return true;
+    }
+}
